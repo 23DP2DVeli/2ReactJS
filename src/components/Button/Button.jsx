0601fcb37e492703b5493e6d53fdc987c7863bae
Daniels <A230255DV@rvt.lv>
2025-09-20 23:47:03 +0300

    OnClick

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,14 @@
 import './Button.css'
 
 export default function Button({ children }) {
-    return <button className='button'>{children}</button>
-}
+    function handleClick() {
+        console.log('Clicked')
+    }
+
+    return (
+     <button className='button' onClick={handleClick}>
+        {children}
+    </button>
+    
+    )
+    }
