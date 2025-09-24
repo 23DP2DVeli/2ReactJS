14dfe2040c196358f96454456d1f60c3dc39f7fb
Daniels <A230255DV@rvt.lv>
2025-09-24 14:03:57 +0300

    Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Header from './components/Header'
 import Section from './components/Section.jsx'
 import DifferencesSection from './components/Differences.jsx'
+import { Fragment } from 'react'
+
 
 export default function App() {
   return (
-    <div>
+    <Fragment>
       <Header />
       <main>
         <Section />
         <DifferencesSection/>
       </main>
-    </div>
+    </Fragment>
 
   )
 }
